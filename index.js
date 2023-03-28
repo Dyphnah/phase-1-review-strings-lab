0ccc5f385548a8b05f2d6944457301df124bd46c
Dyphnah Nyamongo <nyandukodyphnah4@gmail.com>
2023-03-28 08:50:49 +0300

    Completed Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'GRACE HOPPER';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+const firstInitial = "currentUser[0]";
+const restOfName = "currentUser.slice(1)";
+const shortGreeting = `Welcome, ${currentUser[0]}!`;  
